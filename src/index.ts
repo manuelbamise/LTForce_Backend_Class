44bea0b2aaee75a5dedf4abcdb2b2c7b694fe3df
manuelbamise <etobi1930@gmail.com>
2026-06-04 17:54:09 +0100

    progress: new function to the index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-const Ind3: number = 45;
-console.log(Ind3);
+// const list_of_cars: string[] = ['toyota', 'camry', 'lexus', 'tycan'];
 
-const strrr: string = 'intro to typescript';
-console.log(strrr);
+// const var4: string
+
+// interface obj1 {
+//   name: string;
+//   age: number;
+//   isStudent: boolean;
+//   pGrade: grade;
+// }
+
+// enum grade {
+//   js1,
+//   js2,
+//   js3,
+//   ss1,
+//   ss2,
+//   ss3,
+// }
+
+// const obj1: obj1[] = [
+//   {
+//     name: 'ade',
+//     age: 14,
+//     isStudent: false,
+//     pGrade: grade.ss2,
+//   },
+//   {
+//     name: 'mary',
+//     age: 25,
+//     isStudent: true,
+//     pGrade: grade.ss3,
+//   },
+//   {
+//     name: 'felix',
+//     age: 45,
+//     isStudent: true,
+//     pGrade: grade.js3,
+//   },
+// ];
+
+const newFunc = (x: number, y: number): number => {
+  return x + y;
+};
+
+const num1 = 24;
+const num2 = 16;
+
+const result = newFunc(num1, num2);
+
+console.log(typeof result);
